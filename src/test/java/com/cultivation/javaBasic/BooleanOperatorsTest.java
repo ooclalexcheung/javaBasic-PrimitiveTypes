9fb7a2f2982d4fd4fea798a3da790ae45a928d85
ooclalexcheung <41135345+ooclalexcheung@users.noreply.github.com>
2018-11-15 00:24:58 +0800

    bug fix

--- src/test/java/com/cultivation/javaBasic/BooleanOperatorsTest.java
+++ src/test/java/com/cultivation/javaBasic/BooleanOperatorsTest.java
@@ -68,7 +68,7 @@ class BooleanOperatorsTest {
 
         // TODO: please write down the result directly to pass the test.
         // <--start
-        final int expected = -43982;
+        final int expected = -65536;
         // --end-->
 
         assertEquals(expected, ~value);
